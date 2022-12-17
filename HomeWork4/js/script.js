5ba32835a7a82f3fb21acc8eebef76d6e1f59a44
lonaver <ilonaver@ukr.net>
2022-12-17 21:13:50 +0200

    homework4

---
+++ HomeWork4/js/script.js
@@ -0,0 +1,68 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
+const marks = [4, 5, 5, 3, 4, 5];
+//formation of teams: boy and girl
+GetArrayCouple = (students) => {
+  const arrayF = [];
+  const arrayM = [];
+  const arrayLetter = ["а", "я"];
+  for (let value of students) {
+    let name = value.split("");
+    if (arrayLetter.indexOf(name.at(-1), 0) != -1) {
+      arrayF.push(value);
+    } else {
+      arrayM.push(value);
+    }
+  }
+  const ArrayResultCouple = [];
+  for (let i = 0; i < arrayM.length; i++) {
+    ArrayResultCouple.push([arrayM[i], arrayF[i]]);
+  }
+  return ArrayResultCouple;
+};
+const arrayCommand = GetArrayCouple(students);
+
+//get topic for team
+GetTopic = (arrayCommand, themes) => {
+  const ArrayCommandTheme = [];
+  for (let i = 0; i < arrayCommand.length; i++) {
+    let stringCommand = arrayCommand[i][0] + " і " + arrayCommand[i][1];
+    ArrayCommandTheme.push([stringCommand, themes[i]]);
+  }
+  return ArrayCommandTheme;
+};
+const arrayCommandWithTheme = GetTopic(arrayCommand, themes);
+
+//get mark for student
+MarksForStudents = (students, marks) => {
+  const arrayMarkStu = [];
+  for (let i = 0; i < students.length; i++) {
+    arrayMarkStu.push([students[i], marks[i]]);
+  }
+  return arrayMarkStu;
+};
+const arrayMarkForStudednts = MarksForStudents(students, marks);
+
+//get randon mark for student within 1-5 range
+GetMarksCommand = (arrayCommandWithTheme) => {
+  const ArrayCommandThemeMark = [...arrayCommandWithTheme];
+  const max = 5;
+  const min = 1;
+
+  for (let i = 0; i < arrayCommandWithTheme.length; i++) {
+    let RandomNumber = Math.random();
+    let randomMark = Math.floor((max - min) * RandomNumber + min);
+    ArrayCommandThemeMark[i].push(randomMark);
+  }
+  return ArrayCommandThemeMark;
+};
+const arrayCommandMark = GetMarksCommand(arrayCommandWithTheme);
+
+console.log(arrayCommand);
+console.log(arrayCommandWithTheme);
+console.log(arrayMarkForStudednts);
+console.log(arrayCommandMark);
