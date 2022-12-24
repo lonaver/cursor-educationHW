783eb5877e447b7fdc222decd26b2a0b2d66b513
lonaver <ilonaver@ukr.net>
2022-12-24 20:14:52 +0200

    fixed HW2 while

--- HomeWork2/js/script.js
+++ HomeWork2/js/script.js
@@ -1,16 +1,20 @@
 let newNumber;
 let newNumberAn;
 do {
-  do {
-    newNumber = Number(prompt("min number must be integer and positive: "));
-  } while (newNumber < 0 && newNumber !== Math.floor(newNumber));
-  do {
-    newNumberAn = Number(prompt("max number must be integer and positive: "));
-  } while (newNumberAn !== Math.floor(newNumberAn) && newNumberAn < 0);
-} while (newNumber > newNumberAn);
+  newNumber = Number(prompt("min number must be integer and positive: "));
+} while (newNumber < 0 || newNumber !== Math.floor(newNumber));
+do {
+  newNumberAn = Number(
+    prompt("max number must be integer and positive and max>min: ")
+  );
+} while (
+  newNumberAn < 0 ||
+  newNumberAn !== Math.floor(newNumberAn) ||
+  newNumber > newNumberAn
+);
+
 const needEven = confirm("Does it skip even numbers?");
 let sum = 0;
-// debugger;
 for (let i = newNumber; i <= newNumberAn; i++) {
   if (needEven == true && i % 2 !== 0) {
     sum += i;
